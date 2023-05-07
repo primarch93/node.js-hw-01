aab74fc243475afd8630df642db32761b382c81c
primarch93 <sugonegor@gmail.com>
2023-05-07 14:39:58 +0300

    steps-4

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
-const {listContacts, getContactById, removeContact, addContact,} = require('./contacts');
-const { Command } = require("commander");
+const {listContacts, getContactById, removeContact, addContact} = require('./contacts');
+const {Command} = require('commander');
 
 
 const program = new Command();
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторить
-function invokeAction({ action, id, name, email, phone }) {
+
+const invokeAction = async ({action, id, name, email, phone}) => {
   switch (action) {
-    case "list":
-      // ...
+    case 'list':
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
-    case "get":
-      // ... id
+    case 'get':
+      const contactById = await getContactById(id);
+      if (!contactById) {
+        throw new Error(`Contact with this not found`);
+      }
+      console.log(contactById);
       break;
 
-    case "add":
-      // ... name email phone
+    case 'add':
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
-    case "remove":
-      // ... id
+    case 'remove':
+      const removedContact = await removeContact(id);
+      console.log(`Contact with id ${id} was removed from the contact list`);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
-}
+};
 
 invokeAction(argv);
